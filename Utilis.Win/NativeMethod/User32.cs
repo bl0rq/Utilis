90af57fffa178c21c1a24d1deebc19cc0561a3f4
Brad Buhrkuhl <bbuhrkuhl@lundeng.com>
2017-01-08 17:47:33 -0800

    More extern'ed calls

--- Utilis.Win/NativeMethod/User32.cs
+++ Utilis.Win/NativeMethod/User32.cs
@@ -23,5 +23,71 @@ namespace Utilis.Win.NativeMethod
                 return ( ( GetSystemMetrics ( 0x1000 ) & 1 ) != 0 );
             }
         }
+
+        [System.Runtime.InteropServices.DllImport ( "user32.dll" )]
+        public static extern bool SetWindowPlacement ( IntPtr hWnd, [System.Runtime.InteropServices.In] ref WINDOWPLACEMENT lpwndpl );
+
+        [System.Runtime.InteropServices.DllImport ( "user32.dll" )]
+        public static extern bool GetWindowPlacement ( IntPtr hWnd, out WINDOWPLACEMENT lpwndpl );
+    }
+
+
+    [Serializable]
+    [System.Runtime.InteropServices.StructLayout ( System.Runtime.InteropServices.LayoutKind.Sequential )]
+    public struct RECT
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+
+        public RECT ( int left, int top, int right, int bottom )
+        {
+            this.Left = left;
+            this.Top = top;
+            this.Right = right;
+            this.Bottom = bottom;
+        }
+    }
+
+    // POINT structure required by WINDOWPLACEMENT structure
+    [Serializable]
+    [System.Runtime.InteropServices.StructLayout ( System.Runtime.InteropServices.LayoutKind.Sequential )]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+
+        public POINT ( int x, int y )
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+
+    // WINDOWPLACEMENT stores the position, size, and state of a window
+    [Serializable]
+    [System.Runtime.InteropServices.StructLayout ( System.Runtime.InteropServices.LayoutKind.Sequential )]
+    public struct WINDOWPLACEMENT
+    {
+        public int length;
+        public int flags;
+        public int showCmd;
+        public POINT minPosition;
+        public POINT maxPosition;
+        public RECT normalPosition;
+
+        public const int SW_HIDE = 0;
+        public const int SW_SHOWNORMAL = 1;
+        public const int SW_NORMAL = 1;
+        public const int SW_SHOWMINIMIZED = 2;
+        public const int SW_SHOWMAXIMIZED = 3;
+        public const int SW_MAXIMIZE = 3;
+        public const int SW_SHOWNOACTIVATE = 4;
+        public const int SW_SHOW = 5;
+        public const int SW_MINIMIZE = 6;
+        public const int SW_SHOWMINNOACTIVE = 7;
+        public const int SW_SHOWNA = 8;
+        public const int SW_RESTORE = 9;
     }
 }
