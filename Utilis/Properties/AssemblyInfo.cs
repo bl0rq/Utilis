0e325247a1a83fbc54eb1ed0b85cbbd26799bc2b
Brad Buhrkuhl <bbuhrkuhl@lundeng.com>
2017-01-08 17:48:26 -0800

    Version++

--- Utilis/Properties/AssemblyInfo.cs
+++ Utilis/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ( "0.8.1.68" )]
-[assembly: AssemblyFileVersion ( "0.8.1.68" )]
-[assembly: AssemblyInformationalVersion ( "0.8.1.68" )]
+[assembly: AssemblyVersion ( "0.8.1.70" )]
+[assembly: AssemblyFileVersion ( "0.8.1.70" )]
+[assembly: AssemblyInformationalVersion ( "0.8.1.70" )]
 
 [assembly: InternalsVisibleTo ( "Utilis.Win" )]
