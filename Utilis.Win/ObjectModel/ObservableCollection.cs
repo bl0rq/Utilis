31f79fd1155c192029ff9c54335814674a4439cf
Brad Buhrkuhl <brad@blorq.com>
2019-09-18 14:58:01 -0700

    Added AddRange to Observable Collection

--- Utilis.Win/ObjectModel/ObservableCollection.cs
+++ Utilis.Win/ObjectModel/ObservableCollection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utilis.Extensions;
 
 // This was causing stupid issues.  No clue how or why.
 //namespace System.Collections.Specialized
@@ -15,9 +16,10 @@ using System.Threading.Tasks;
 //}
 namespace Utilis.ObjectModel
 {
-    public interface INotifyCollectionChanged<T> : ICollection<T>, System.Collections.Specialized.INotifyCollectionChanged
+    public interface INotifyCollectionChanged<T> : ICollection<T>, System.Collections.Specialized.INotifyCollectionChanged, System.ComponentModel.INotifyPropertyChanged
     {
-
+        void AddRange ( IList<T> items );
+        void AddRange ( T [] items );
     }
 
     public class ObservableCollection<T> : System.Collections.ObjectModel.ObservableCollection<T>, INotifyCollectionChanged<T>
@@ -38,6 +40,51 @@ namespace Utilis.ObjectModel
 
         private event System.Collections.Specialized.NotifyCollectionChangedEventHandler CollectionChangedNormal;
         private event System.Collections.Specialized.NotifyCollectionChangedEventHandler CollectionChangedDispatcher;
+
+        bool m_isInAddRange = false;
+        public void AddRange ( IList<T> items )
+        {
+            if ( m_isInAddRange )
+                throw new NotSupportedException ( "Another add-range is already in progress!" );
+
+            m_isInAddRange = true;
+
+            foreach ( var item in items )
+            {
+                Add ( item );
+            }
+
+            var dlgCollectionChangedDispatcher = CollectionChangedDispatcher;
+            if ( dlgCollectionChangedDispatcher != null )
+                Runner.RunOnDispatcherThreadBlocking (
+                    ( ) =>
+                    {
+                        foreach ( var item in items )
+                        {
+                            dlgCollectionChangedDispatcher (
+                                this,
+                                new System.Collections.Specialized.NotifyCollectionChangedEventArgs ( System.Collections.Specialized.NotifyCollectionChangedAction.Add, item ) );
+                        }
+                    } );
+
+            var dlgCollectionChangedNormal = CollectionChangedNormal;
+            if ( dlgCollectionChangedNormal != null )
+            {
+                foreach ( var item in items )
+                {
+                    dlgCollectionChangedNormal (
+                        this,
+                        new System.Collections.Specialized.NotifyCollectionChangedEventArgs ( System.Collections.Specialized.NotifyCollectionChangedAction.Add, item ) );
+                }
+            }
+
+            m_isInAddRange = false;
+        }
+
+        public void AddRange ( T [] items )
+        {
+            AddRange ( (IList<T>)items );
+        }
 
         public override event System.Collections.Specialized.NotifyCollectionChangedEventHandler CollectionChanged
         {
@@ -60,6 +107,9 @@ namespace Utilis.ObjectModel
         private readonly object m_collectionChangedLock = new object ( );
         protected override void OnCollectionChanged ( System.Collections.Specialized.NotifyCollectionChangedEventArgs e )
         {
+            if ( m_isInAddRange )
+                return;
+
             // one at a time or things get confusing.  
             //TODO: Could possibly queue them to keep from blocking?
             lock ( m_collectionChangedLock )
@@ -72,6 +122,30 @@ namespace Utilis.ObjectModel
                 if ( dlgCollectionChangedNormal != null )
                     dlgCollectionChangedNormal ( this, e );
             }
+        }
+
+        protected bool SetProperty<T> ( ref T oldValue, T newValue, [System.Runtime.CompilerServices.CallerMemberName] string propertyName = "" )
+        {
+            bool areDifferent = !object.Equals ( oldValue, newValue );
+            oldValue = newValue;
+
+            if ( areDifferent )
+            {
+                OnPropertyChanged ( propertyName );
+            }
+
+            return areDifferent;
+        }
+        protected void OnPropertyChanged<T> ( System.Linq.Expressions.Expression<Func<T>> exp )
+        {
+            OnPropertyChanged ( exp.GetName ( ) );
+        }
+
+        protected void OnPropertyChanged ( string name )
+        {
+            var e = new System.ComponentModel.PropertyChangedEventArgs ( name );
+
+            OnPropertyChanged ( e );
         }
     }
 }
