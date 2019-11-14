1f3d2ae6b46728a879a03f1444dcbeed56b2059d
Brad Buhrkuhl <brad@blorq.com>
2019-11-14 12:06:39 -0800

    Add attached properties to use for Glyph fill and stroke

--- Utilis.Win/UI/Properties/Glyph.cs
+++ Utilis.Win/UI/Properties/Glyph.cs
@@ -12,14 +12,48 @@ namespace Utilis.UI.Win.Properties
         {
             return (System.Windows.Media.Geometry)obj.GetValue ( DataProperty );
         }
+
         public static void SetData ( System.Windows.DependencyObject obj, System.Windows.Media.Geometry value )
         {
             obj.SetValue ( DataProperty, value );
         }
+
         public static readonly System.Windows.DependencyProperty DataProperty =
             System.Windows.DependencyProperty.RegisterAttached (
                 "Data",
                 typeof ( System.Windows.Media.Geometry ),
+                typeof ( Glyph ),
+                new System.Windows.PropertyMetadata ( null ) );
+
+        public static System.Windows.Media.Brush GetFill ( System.Windows.DependencyObject obj )
+        {
+            return (System.Windows.Media.Brush)obj.GetValue ( FillProperty );
+        }
+
+        public static void SetFill ( System.Windows.DependencyObject obj, System.Windows.Media.Brush value )
+        {
+            obj.SetValue ( FillProperty, value );
+        }
+        public static readonly System.Windows.DependencyProperty FillProperty =
+            System.Windows.DependencyProperty.RegisterAttached (
+                "Fill",
+                typeof ( System.Windows.Media.Brush ),
+                typeof ( Glyph ),
+                new System.Windows.PropertyMetadata ( null ) );
+
+        public static System.Windows.Media.Brush GetStroke ( System.Windows.DependencyObject obj )
+        {
+            return (System.Windows.Media.Brush)obj.GetValue ( StrokeProperty );
+        }
+
+        public static void SetStroke ( System.Windows.DependencyObject obj, System.Windows.Media.Brush value )
+        {
+            obj.SetValue ( StrokeProperty, value );
+        }
+        public static readonly System.Windows.DependencyProperty StrokeProperty =
+            System.Windows.DependencyProperty.RegisterAttached (
+                "Stroke",
+                typeof ( System.Windows.Media.Brush ),
                 typeof ( Glyph ),
                 new System.Windows.PropertyMetadata ( null ) );
     }
