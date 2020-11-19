16078b81e65791a28ad4c580367f2f46c0b27b96
Brad Buhrkuhl <brad@blorq.com>
2020-11-19 15:08:14 -0800

    Update PCL to dotnet core

--- Utilis/ServiceLocator.cs
+++ Utilis/ServiceLocator.cs
@@ -8,86 +8,89 @@ using Utilis.Extensions;
 
 namespace Utilis
 {
-	public interface IServiceLocator : Microsoft.Practices.ServiceLocation.IServiceLocator, Messaging.IListener<Messaging.AppShutdownMessage>
-	{
-		void RegisterInstance<T> ( T o );
-		void RemoveInstance<T> ( );
-	}
+    public interface IServiceLocator :
 
-	public class ServiceLocator : Microsoft.Practices.ServiceLocation.ServiceLocatorImplBase, IServiceLocator
-	{
-		public static IServiceLocator Instance
-		{
-			get;
-			set;
-		}
+        System.IServiceProvider,
+        CommonServiceLocator.IServiceLocator,
+        Messaging.IListener<Messaging.AppShutdownMessage>
+    {
+        void RegisterInstance<T> ( T o );
+        void RemoveInstance<T> ( );
+    }
 
+    public class ServiceLocator : CommonServiceLocator.ServiceLocatorImplBase, IServiceLocator, Messaging.IListener<Messaging.AppShutdownMessage>
+    {
+        public static IServiceLocator Instance
+        {
+            get;
+            set;
+        }
 
-		private readonly Autofac.IComponentContext m_context;
-		private IDisposable m_busToken;
+        private readonly Autofac.IContainer m_container;
+        private IDisposable m_busToken;
 
-		private readonly Dictionary<Type, object> m_additionalTypes = new Dictionary<Type, object> ( );
+        private readonly Dictionary<Type, object> m_additionalTypes = new Dictionary<Type, object> ( );
 
-		public ServiceLocator ( Autofac.IComponentContext context )
-		{
-		    Contract.AssertNotNull(() => context, context);
+        public ServiceLocator ( Autofac.IContainer container ) //: base ( container.BeginLifetimeScope ( ) )
+        {
+            Contract.AssertNotNull ( ( ) => container, container );
 
-			m_context = context;
+            m_container = container;
 
-			m_busToken = Messaging.Bus.Instance.ListenFor ( this );
-		}
+            m_busToken = Messaging.Bus.Instance.ListenFor ( this );
+        }
 
-		protected override object DoGetInstance ( Type serviceType, string key )
-		{
-			return
-				m_additionalTypes.SafeGet ( serviceType )
-				?? ( key != null
-					? m_context.Resolve ( serviceType, new [] { new NamedParameter ( "key", key ) } )
-					: m_context.SafeResolve ( serviceType ) );
-		}
+        protected override object DoGetInstance ( Type serviceType, string key )
+        {
+            return
+                m_additionalTypes.SafeGet ( serviceType )
+                ?? ( key != null
+                    ? m_container.Resolve ( serviceType, new [] { new NamedParameter ( "key", key ) } )
+                    : m_container.SafeResolve ( serviceType ) );
+        }
 
-		protected override IEnumerable<object> DoGetAllInstances ( Type serviceType )
-		{
-			var enumerableType = typeof ( IEnumerable<> ).MakeGenericType ( serviceType );
+        protected override IEnumerable<object> DoGetAllInstances ( Type serviceType )
+        {
+            var enumerableType = typeof ( IEnumerable<> ).MakeGenericType ( serviceType );
 
-			object instance = m_context.Resolve ( enumerableType );
-			return ( (System.Collections.IEnumerable)instance ).Cast<object> ( );
-		}
+            object instance = m_container.Resolve ( enumerableType );
+            return ( (System.Collections.IEnumerable)instance ).Cast<object> ( );
+        }
 
-		public void Receive ( Messaging.AppShutdownMessage message )
-		{
-			m_busToken.DisposeWithCare ( );
-			m_busToken = null;
+        public void Receive ( Messaging.AppShutdownMessage message )
+        {
+            m_busToken.DisposeWithCare ( );
+            m_busToken = null;
 
-			m_context.ComponentRegistry.DisposeWithCare ( );
-		}
+            m_container.ComponentRegistry.DisposeWithCare ( );
+        }
 
-		public void RegisterInstance<T> ( T o )
-		{
-			lock ( m_additionalTypes )
-			{
-				m_additionalTypes [ typeof ( T ) ] = o;
-			}
-		}
+        public void RegisterInstance<T> ( T o )
+        {
+            lock ( m_additionalTypes )
+            {
+                m_additionalTypes [ typeof ( T ) ] = o;
+            }
+        }
 
-		public void RemoveInstance<T> ( )
-		{
-			lock ( m_additionalTypes )
-			{
-				m_additionalTypes.Remove ( typeof ( T ) );
-			}
-		}
-	}
+        public void RemoveInstance<T> ( )
+        {
+            lock ( m_additionalTypes )
+            {
+                m_additionalTypes.Remove ( typeof ( T ) );
+            }
+        }
+    }
 
-	public static class ComponentContextExtensions
-	{
-		public static object SafeResolve ( this Autofac.IComponentContext context, Type serviceType )
-		{
-			object o;
-			if ( context.TryResolve ( serviceType, out o ) )
-				return o;
-			else
-				return null;
-		}
-	}
+    public static class ComponentContextExtensions
+    {
+        public static object SafeResolve ( this Autofac.IComponentContext context, Type serviceType )
+        {
+            object o;
+            if ( context.TryResolve ( serviceType, out o ) )
+                return o;
+            else
+                return null;
+        }
+    }
 }
